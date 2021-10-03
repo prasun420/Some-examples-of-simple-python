c7df224db2092a9906a3a28e9a6632498be7161d
Debjani12 <91781087+Debjani12@users.noreply.github.com>
2021-10-03 16:28:06 +0530

    Add files via upload

---
+++ negpos.py
@@ -0,0 +1,7 @@
+n=int(input(" Enter a number:"))
+if n==0:
+    print(" The number entered is zero")
+elif(n<0):
+    print(" The entered number is negative")
+else:
+    print(" Entered number is positive")  
